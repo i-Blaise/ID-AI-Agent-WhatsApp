7b9c4a950824f949c022a1a7c136a79a0328b097
i-Blaise <menniablaise@yahoo.com>
2025-06-13 16:46:18 +0000

    Fix typo in logging configuration

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Configure logging
-t_logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
 
 app = Flask(__name__)
 
